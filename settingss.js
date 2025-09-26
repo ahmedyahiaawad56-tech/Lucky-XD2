f036d3bfc0032fb8b47538f341cd0356f59ec08d
ahmedyahiaawad56-tech <ahmedyahiaawad56@gmail.com>
2025-09-26 11:58:26 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "trend-x~eggkTQIa#0EqWZp0czCkM0914BArGd0CYRRbkaanzinutn1UeBUw",  // add your Session Id here
+  SESSION_ID: "",  // add your Session Id here
   OWNER_NUMBER: "249991519998", // put your phone number here
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   TIMEZONE: "Africa/Kampala" //put your country timeZone....leave blank if u don't know.
