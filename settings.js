5c40ae019187f2d9f86c97140941f34ce00cff81
ahmedyahiaawad56-tech <ahmedyahiaawad56@gmail.com>
2025-09-30 19:56:33 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ module.exports = {
   OWNER_NUMBER: settings.OWNER_NUMBER || process.env.OWNER_NUMBER || "249991519998",
   OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "ʟᴜᴄᴋʏ ➋➊➑",
   DEV: process.env.DEV || "249991519998",
-  DEVELOPER_NUMBER: '256789966218@s.whatsapp.net',
+  DEVELOPER_NUMBER: '249991519998@s.whatsapp.net',
   MENU_AUDIO_URL: process.env.MENU_AUDIO_URL || 'https://files.catbox.moe/3v5i11.mp3',
 NEWSLETTER_JID: process.env.NEWSLETTER_JID || '120363420656466131@newsletter',
 
